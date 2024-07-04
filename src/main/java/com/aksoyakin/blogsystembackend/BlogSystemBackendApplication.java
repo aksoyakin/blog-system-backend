13abbaffaccca1eb9407450c541ebee536ab466f
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-07-04 19:04:38 +0300

    first commit

---
+++ src/main/java/com/aksoyakin/blogsystembackend/BlogSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aksoyakin.blogsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aksoyakin/blogsystembackend/BlogSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksoyakin.blogsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
